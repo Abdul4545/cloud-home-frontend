35b9dc8a8816e41d6107f31a782be7576d4d621c
Abdul Moid <abdulmoidkhan221@gmail.com>
2024-07-28 10:08:56 +0530

    Update signupPage.js added regex

--- src/pages/signupPage.js
+++ src/pages/signupPage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const SignupPage = () => {
 
     const validateEmail = (email) => {
-        const EMAIL_REGEX = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+        const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         return EMAIL_REGEX.test(String(email).toLowerCase());
     };
 
